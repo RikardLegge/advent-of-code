0d8d3e9e0fcb7b87a19a85fa7fd88d258836c8b8
Rikard Legge <rikard.legge@gmail.com>
2019-12-06 15:42:21 +0100

    Solve day 2b 2019

--- 2019/src/Day2.hs
+++ 2019/src/Day2.hs
@@ -6,12 +6,24 @@ module Day2
 import Control.Monad
 
 run1 :: String -> IO ()
-run1 input = putStrLn (show list ++ "\n" ++ show (run 0 list))
+run1 input = print (head (run 0 list))
   where
     list = set (set (map read (split input)) 1 12) 2 2
 
 run2 :: String -> IO ()
-run2 input = print "Not done"
+run2 input = print (show noun ++ "*100+" ++ show verb ++ " = " ++ show (100 * noun + verb))
+  where
+    list = map read (split input)
+    (noun, verb, res) = findReplace 0 0 list
+
+findReplace :: Int -> Int -> [Int] -> (Int, Int, [Int])
+findReplace noun verb list
+  | head res == 19690720 = (noun, verb, list)
+  | noun == 100 = findReplace 0 (verb + 1) list
+  | noun < 100 && verb <= 100 = findReplace (noun + 1) verb list
+  | otherwise = (noun, verb, res)
+  where
+    res = run 0 (set (set list 1 noun) 2 verb)
 
 run :: Int -> [Int] -> [Int]
 run offset list = case res of
