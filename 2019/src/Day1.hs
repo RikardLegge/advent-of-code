d785b89d55819c51d11edf21cd2ef22ba04f0655
Rikard Legge <rikard.legge@gmail.com>
2019-12-09 09:48:06 +0100

    Improve day1b 2019

--- 2019/src/Day1.hs
+++ 2019/src/Day1.hs
@@ -4,16 +4,15 @@ module Day1
     ) where
 
 run1 :: String -> IO ()
-run1 input = print (sum (map (fuel . read) (words input)))
+run1 input = print $ sum $ map (fuel . read) (words input)
 
 run2 :: String -> IO ()
-run2 input = print (sum (map (recFuel . read) (words input)))
+run2 input = print $ sum $ map (recFuel . read) (words input)
 
 fuel :: Int -> Int
 fuel weight = max (div weight 3 - 2) 0
 
 recFuel :: Int -> Int
-recFuel weight 
-  | f == 0 = 0
-  | otherwise = f + recFuel f
+recFuel 0 = 0 
+recFuel weight = f + recFuel f
   where f = fuel weight
